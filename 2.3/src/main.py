a4e7fb40e1acc7d46c520b8eca774fc237376314
Walkiiiy <95386260+Walkiiiy@users.noreply.github.com>
2023-12-27 19:09:13 +0800

    add_2.3

---
+++ 2.3/src/main.py
@@ -0,0 +1,101 @@
+import horizontal
+import vertical
+import split
+import cv2
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+import matplotlib.pyplot as plt
+import ocrModule.Ocr as ocr
+
+img_path = '/home/walkiiiy/Zensplit/2.3/src/rawpic/1.JPG'  # 设置图像路径，必须是相对路径且不能有中文
+txtpath = '/home/walkiiiy/Zensplit/2.3/src/ocr_outcome.txt'
+outcomepath = "/home/walkiiiy/Zensplit/2.3/src/outcome/outcome.jpg"
+
+
+def cv2AddChineseText(img, text, position, textColor=(0, 0, 0), textSize=15):
+    if (isinstance(img, np.ndarray)):  # 判断是否OpenCV图片类型
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    # 创建一个可以在给定图像上绘图的对象
+    draw = ImageDraw.Draw(img)
+    # 字体的格式
+    fontStyle = ImageFont.truetype(
+        "simsun.ttc", textSize, encoding="utf-8")
+    # 绘制文本
+    draw.text(position, text, textColor, font=fontStyle)
+    # 转换回OpenCV格式
+    return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+
+
+if __name__ == '__main__':
+    ocrRes = ocr.createRequest(img_path)
+
+    with open(txtpath, 'w') as file:
+        for line in ocrRes:
+            file.write(line)
+
+    with open(txtpath, 'r') as file:
+        # 读取文件内容到一个字符串
+        ocr_content_raw = file.read()
+        print('api返回字数:', len(ocr_content_raw))
+        ocr_content_store = ocr_content_raw
+
+    # 投影法图像分割
+    img_bgr = cv2.imread(img_path, 1)
+    if not img_bgr is None:
+        img = img_bgr.copy()
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # 二值化
+        t, binary = cv2.threshold(
+            img_gray, 0, 255, cv2.THRESH_OTSU+cv2.THRESH_BINARY)
+    else:
+        print("couldn't read image!")
+    lines = horizontal.get_horizonal(binary)
+    height, width = binary.shape
+    ordinats = []
+    char_num = 0
+    print('投影分割行数：', len(lines))
+
+    for line in lines:
+        img_hor = binary[line[0]:line[1], :]
+        h_list = vertical.get_vertical_all(img_hor)  # 第一趟
+        char_num += len(h_list)
+        for x_ordinate in h_list:
+            ordinats.append((x_ordinate, line))
+    print("first scan,num of char:", len(ordinats))
+    # print(ordinats)
+
+    char_width = split.get_inform(ordinats)
+    print(char_width)
+
+    ordinats_Zen = []
+    char_num_Zen = 0
+    row_num = 0  # 行数
+    for line in lines:
+        img_hor = binary[line[0]:line[1], :]
+        h_list_Zen = vertical.get_vertical_Zen(
+            img_hor, char_width, ocrRes[row_num])  # 第二趟
+        row_chr_num = len(h_list_Zen)  # 本行字数
+        i = 0
+        for x_ordinate in h_list_Zen:
+            ordinate = (x_ordinate, line)
+            ordinats_Zen.append(ordinate)
+            # 打印文字
+            try:
+                img_bgr = cv2AddChineseText(img_bgr, ocrRes[row_num][i], ((
+                    ordinate[0][0]+ordinate[0][1])//2, ordinate[1][0]-(ordinate[0][1]-ordinate[0][0])//2))
+            except IndexError:
+                pass
+            i += 1
+        row_num += 1
+    print("second scan,num of char:", len(ordinats_Zen))
+
+    # 打印字符框
+    img_res = img_bgr
+    for ordinate in ordinats_Zen:
+        cv2.rectangle(img_res, (ordinate[0][0], ordinate[1][0]),
+                      (ordinate[0][1], ordinate[1][1]), (255, 0, 0), 1)
+
+    # 显示和存储图片
+    # cv2.imshow('outcome',img_cut)
+    # cv2.waitKey(0)
+    cv2.imwrite(outcomepath, img_res)
